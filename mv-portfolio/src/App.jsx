963fd97fd21923158dc252d20f9c79cfb5903a9e
Mostovyi Viktor <74181645+Vitamin4uk@users.noreply.github.com>
2025-03-20 14:15:19 +0200

    projects

--- mv-portfolio/src/App.jsx
+++ mv-portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import HeroGradient from "./components/heroSection/HeroGradient";
 import HeroMain from "./components/heroSection/HeroMain";
 import SubHeroSection from "./components/heroSection/SubHeroSection";
 import NavbarMain from "./components/navbar/NavbarMain";
+import ProjectsMain from "./components/projectsSection/ProjectsMain";
 import SkillsMain from "./components/skillSection/SkillsMain";
 import SubSkills from "./components/skillSection/SubSkills";
 
@@ -19,6 +20,7 @@ function App() {
       <SkillsMain />
       <SubSkills />
       <ExperienceMain />
+      <ProjectsMain />
       <HelperSection />
     </main>
   );
